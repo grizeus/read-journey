2ea6e322d3009009a66e97c87bae8050291f6f54
grizeus <zaytsewww@gmail.com>
2025-05-07 13:36:39 +0300

    add necessary fonts

--- src/components/pages/Reading.tsx
+++ src/components/pages/Reading.tsx
@@ -1,5 +1,5 @@
 const ReadingPage = () => {
-  return <div>Reading Page</div>;
+  return <div className="font-bold text-[28px] leading-8 tracking-[0.02em]">Recommended Page</div>;
 };
 
 export default ReadingPage;
